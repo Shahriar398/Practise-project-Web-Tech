8105713eca670090ce91a606daaceb3671b49242
M. Shahriar Hasan Limon <shahriarh398@gmail.com>
2026-08-16 08:52:51 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+<title>Home</title>
+</head>
+ 
+<body>
+ 
+<h1 align="center">
+Student Management System
+</h1>
+ 
+<hr>
+ 
+<br><br><br>
+ 
+<center>
+ 
+<h2>Welcome</h2>
+ 
+<br>
+ 
+<a href="login.php">
+<input type="button" value="Login">
+</a>
+ 
+</center>
+ 
+<br><br><br>
+ 
+<hr>
+ 
+<p align="center">
+ 
+Copyright &copy;
+<?php echo date("Y"); ?>
+ 
+</p>
+ 
+</body>
+ 
+</html>
