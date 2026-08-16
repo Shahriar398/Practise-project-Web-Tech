1856b1e1b4c0f2e11493726414c073633df872db
M. Shahriar Hasan Limon <shahriarh398@gmail.com>
2026-08-16 08:52:37 +0600

    Add files via upload

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+ 
+$conn = mysqli_connect("localhost","root","","un1");    
+                   //   Server  Username Password Database
+if(!$conn)
+{
+    die("Connection Failed");
+}
+ 
+?>
